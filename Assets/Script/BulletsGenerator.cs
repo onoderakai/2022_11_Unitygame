43142e2b4399db401d06738d970767e5f0f45813
onoderakai <k022g1011@g.neec.ac.jp>
2022-10-28 21:17:16 +0900

    test弾の処理

--- Assets/Script/BulletsGenerator.cs
+++ Assets/Script/BulletsGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BulletsGenerator : MonoBehaviour
 {
+    int bulletsCoolTime = 30;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class BulletsGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) && bulletsCoolTime <= 0)
+        {
+            bulletsCoolTime = 30;
+
+        }
+        if(bulletsCoolTime > 0)
+        {
+            bulletsCoolTime--;
+        }
     }
 }
