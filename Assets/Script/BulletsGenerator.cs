4c5ebbf8072c0a700ba6aa075d419998326c3616
onoderakai <k022g1011@g.neec.ac.jp>
2022-10-30 18:29:20 +0900

    弾の発射

--- Assets/Script/BulletsGenerator.cs
+++ Assets/Script/BulletsGenerator.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class BulletsGenerator : MonoBehaviour
 {
-    int bulletsCoolTime = 30;
+    //ゲームオブジェクトを取得
+    GameObject player;
+    public GameObject bulletPrefab;
+
+    int bulletsCoolTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.player = GameObject.Find("player");
     }
 
     // Update is called once per frame
@@ -18,7 +22,10 @@ public class BulletsGenerator : MonoBehaviour
         if (Input.GetKey(KeyCode.Space) && bulletsCoolTime <= 0)
         {
             bulletsCoolTime = 30;
+            Vector3 pos = player.transform.position;
 
+            GameObject Shot = Instantiate(bulletPrefab) as GameObject;
+            Shot.transform.position = new Vector3(pos.x, pos.y + 13.27f, pos.z + 4.0f);
         }
         if(bulletsCoolTime > 0)
         {
--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     GameObject player;
+    Vector2 vibration = Vector2.zero;
+    Vector2 maxVibration;
+    Vector2 vibrationRange = Vector2.zero;
+    bool flag = false;
 
     // Start is called before the first frame update
     void Start()
     {
         this.player = GameObject.Find("player");
+        maxVibration.x = 1.0f;
+        maxVibration.y = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 playerPos = this.player.transform.position;
-        transform.position = new Vector3(playerPos.x, playerPos.y + 20.0f, playerPos.z - 8.0f);
+        transform.position = new Vector3(playerPos.x + vibration.x, playerPos.y + 20.0f + vibration.y, playerPos.z - 8.0f);
+        if (Input.GetKeyDown(KeyCode.Return) && !flag)
+        {
+            flag = true;
+            vibrationRange = maxVibration;
+        }
+        else if (Input.GetKeyDown(KeyCode.Return)){
+            flag = false;
+        }
+        if (flag)
+        {
+            vibration.x = Random.Range(-vibrationRange.x, vibrationRange.x);
+            vibration.y = Random.Range(-vibrationRange.y, vibrationRange.y);
+            if (vibrationRange.x > 0)
+            {
+                vibrationRange.x -= 0.1f;
+            }
+            if (vibrationRange.y > 0)
+            {
+                vibrationRange.y -= 0.1f;
+            }
+        }
     }
 }
